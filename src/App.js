15be4aeb4a7db0312c320e401f35d0cf897684d4
kuler876 <107093599+kuler876@users.noreply.github.com>
2023-03-05 05:43:17 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuid4 } from 'uuid'
+import { randomColor } from 'randomcolor'
+import Draggable from 'react-draggable'
+
 import './App.css';
 
 function App() {
+  const [item, setItem] = useState('')
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  )
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items))
+  }, [items])
+
+  const newItem = () => {
+    if (item.trim() !== '') {
+      const newItem = {
+        id: uuid4(),
+        item,
+        color: randomColor({
+          luminosty: 'light'
+        }),
+        defaultPos: {
+          x: 200,
+          y: -200
+        }
+      }
+      setItems((items) => [...items, newItem])
+      setItem('')
+    } else {
+      alert('Введите текст...')
+      setItem('')
+    }
+  }
+
+  const deleteNode = (id) => {
+    setItems(items.filter((item) => item.id !== id))
+  }
+
+  const updatePos = (date, index) => {
+    let newArray = [...items]
+    newArray[index].defaultPos = { x: date.x, y: date.y }
+    setItems(newArray)
+  }
+
+  const keyPress = (e) => {
+    const code = e.keyCode || e.which
+    if (code === 13) {
+      newItem()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+        <textarea value={item} type='text' placeholder='Добавить цитату'
+          onChange={(e) => setItem(e.target.value)}
+          onKeyPress={(e) => keyPress(e)}
+        ></textarea>
+        <button className='btn enter' onClick={newItem}>Добавить</button>
+      </div>
+
+      {
+        items.map((item, index) => {
+          return (
+            <Draggable key={index} defaultPosition={item.defaultPos}
+              onStop={(_, date) => {
+                updatePos(date, index)
+              }}
+            >
+              <div className='quote-item' style={{ backgroundColor: item.color }}>
+                <p>
+                  {`${item.item}`}
+                </p>
+
+                <button className='delete' onClick={() => deleteNode(item.id)}>
+                  X
+                </button>
+              </div>
+            </Draggable>
+          )
+        })
+      }
     </div>
   );
 }
